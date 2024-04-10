9dfe2323c4eb2a038576f62b3d47647021a33d6f
andrew godeke <legodeke@hotmail.com>
2024-04-09 20:36:18 -0400

    refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # No need to find the task here since it's already set by before_action
   end
 
   def new
@@ -22,26 +24,27 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # No need to find the task here since it's already set by before_action
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to @task, notice:
+      redirect_to @task, notice: "Task was successfully updated."
     else
       render :edit
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
-    redirect_to tasks_url
+    redirect_to tasks_url, notice: "Task was successfully destroyed."
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
